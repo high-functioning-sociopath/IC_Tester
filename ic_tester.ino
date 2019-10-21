ae6d58a522b126a64359064a0d20c0b225381857
you-know-me <twinkle99@rocketmail.com>
2019-10-21 22:17:52 +0530

    Prettified the code and added pin numbers and implementation for NAND Gate

--- ic_tester.ino
+++ ic_tester.ino
@@ -2,19 +2,35 @@
 #include <Keypad.h> //4x4
 #include <Wire.h> // Library for I2C communication
 
-//#define HIGH 1
-//#define LOW 0
-
+//*******************************************************************************************************//
+                                              //IC MACROS & DECLARATIONS 
 //TTL Gates
 #define QuadNAND "7400"
 #define QuadNOR "7402"
 #define QuadAND "7408"
 #define QuadOR "7432"
-#define HexaNOT "7404"
+#define HexaNOT "7404"                                          
+//*******************************************************************************************************//
+                                              // IC_I/O & TRUTH TABLE
+                                              
+//int Pin14[14]={30,31,32,33,34,35,36,37,38,39,40,41,42,43};             // 14 Pin IC
+      //14pin IC -> VCC: pin 31 ///GND : pin 42
+//int Pin16[16]={30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45};        //16 Pin IC
+      //16pin IC -> VCC: pin 31 ///GND : pin 44
 
-//define LCD pins
+      //^Not necessart....since the pins have already been declared for individual ICs based on i/p and o/p configurations
 
-//keypad
+//input A and B of truth table
+const char *A[] = {"LOW","LOW","HIGH","HIGH"};
+const char *B[] = {"LOW","HIGH","LOW","HIGH"};
+
+const char *OR_output[4] = {"LOW","HIGH","HIGH","HIGH"};
+const char *NAND_output[4] = {"HIGH","HIGH","HIGH","LOW"};
+const char *NOR_output[4] = {"HIGH","LOW","LOW","LOW"};
+const char *AND_output[4] = {"LOW","LOW","LOW","HIGH"};           
+                                 
+//*******************************************************************************************************//
+                                             //KEYPAD
 const byte ROWS = 4; 
 const byte COLS = 4; 
 
@@ -30,28 +46,19 @@ byte colPins[COLS] = {5, 4, 3, 2};
 //A -> Enter //B -> Back
 //C-> EraseAll //D -> think!
 
-//insert the decided pins 
-int Pin14[14]={0,0,0,0,0,0,0,0,0,0,0,0,0,0};             // 14 Pin IC
-int Pin16[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};        //16 Pin IC
+Keypad key = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+                                             
+//*******************************************************************************************************//
+                                            //LCD_DISPLAY
+                                            
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  //  to set the LCD I2C address
+                                           
+//*******************************************************************************************************//
 
-//input A and B of truth table
-const char *A[] = {"LOW","LOW","HIGH","HIGH"};
-const char *B[] = {"LOW","HIGH","LOW","HIGH"};
-
-const char *OR_output[4] = {"LOW","HIGH","HIGH","HIGH"};
-const char *NAND_output[4] = {"HIGH","HIGH","HIGH","LOW"};
-const char *NOR_output[4] = {"HIGH","LOW","LOW","LOW"};
-const char *AND_output[4] = {"LOW","LOW","LOW","HIGH"};
-
-
-String IC_Val = "";
+String IC_Val = "";   //To store the Input IC_Number
 
 void setup(){
-  // Initiate the LCD:
-  LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  //  to set the LCD I2C address
-  // Initialize the Keypad
-  Keypad key = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-  
+   
   lcd.init();
   lcd.backlight();
   
@@ -112,8 +119,9 @@ void loop(){
   
 }
 
+//***********************************************************************************************************************//
+
 //define input and ouptut pins for each IC
-void QuadNAND(){
 //Gate 1: //i/p 1: 
           //i/p 2:
           //o/p 1:
@@ -126,13 +134,53 @@ void QuadNAND(){
 //Gate 4: //i/p 1: 
           //i/p 2:
           //o/p 1:
+
+
+//        VCC   G1_A  G1_B G1_O G3_A  G3_B G3_O  
+
+//          |   |     |     |     |     |     |          
+//         -------------------------------------
+//         -                                   -
+//         ---|                                -
+//         ---|         IC 74XX                -
+//         ---|                                -
+//         ---|                                -
+//         -                                   -
+//         -------------------------------------
+//          |   |     |     |     |     |     |
+//        G2_A  G2_B G2_O G4_A  G4_B G4_O   GND
+
+          //G1 - G4 : Gate 1 to 4 of IC 
+
+          
 //input array = [] -> pin definition
 //output array = [] -> pin definition
 //Set VCC and GROUND
 //set pinmode as output for i/p array
 //and input for o/p array
 //call check TT(i/p array, size of input array, o/p array, size of output array)
- 
+
+//***********************************************************************************************************************//
+
+void QuadNAND(){
+int IC_input[8] = {33,35,30,32,39,41,36,38};    
+int IC_output[4] = {37,34,43,40};
+int Vcc = 31;
+int Gnd = 42;
+
+pinmode(Vcc,OUTPUT);
+pinmode(Gnd,OUTPUT);
+
+digitalWrite(Vcc,HIGH);
+digitalWrite(Gnd,LOW);
+
+for (int p = 0; p < 8; p++){
+  pinmode(IC_input[p],OUTPUT);
+  if(p <4){
+    pinmode(IC_output[p],INPUT);
+  }
+}
+checkTT(IC_input[],8,IC_output[],4,NAND_output[]);
 }
 void QuadNOR(){}
 void QuadAND(){}
